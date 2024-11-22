03549426059e284b9a7f1784268c0465449432c9
Abhishek Sharma <biz.abhisharma@gmail.com>
2024-11-22 18:45:11 +0530

    feat: added restaurant features api

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING as string)
   .then(() => console.log("Connected to db"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
@@ -17,6 +25,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(8080, () => {
   console.log("Server started on port 8080");
